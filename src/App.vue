0b138990885d8f1b23afafeb222ddfab31f47fca
Harvesty <sohoearny@gmail.com>
2020-11-08 16:08:22 +0800

    Add icon libary

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,19 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <DeckList/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import DeckList from './components/DeckList';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    DeckList,
   },
 
   data: () => ({
--- src/components/DeckList.vue
+++ src/components/DeckList.vue
@@ -1,60 +1,58 @@
 <template>
-  <v-container>
+  <v-card>
+    <v-toolbar
+      color="primary"
+      dark
+      flat
+    >
+      <v-icon>mdi-silverware</v-icon>
+      <v-toolbar-title>Local hotspots</v-toolbar-title>
+    </v-toolbar>
+
     <v-row>
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
-        <button v-on:click="greet">Greet</button>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-row>
-          <!-- <v-text-field :value="decks"></v-text-field> -->
+      <v-col>
+        <v-card-text>
           <v-treeview
             :items="decks"
-            open-all
+            selected-color="indigo"
+            open-on-click
+            selectable
+            return-object
             expand-icon="mdi-chevron-down"
             on-icon="mdi-bookmark"
             off-icon="mdi-bookmark-outline"
             indeterminate-icon="mdi-bookmark-minus"
-          ></v-treeview>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
           >
-            {{ link.text }}
-          </a>
-        </v-row>
+          </v-treeview>
+        </v-card-text>
       </v-col>
+
     </v-row>
-  </v-container>
+
+    <v-divider></v-divider>
+
+    <v-card-actions>
+      <v-btn
+        text
+        
+      >
+        Reset
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        class="white--text"
+        color="green darken-1"
+        depressed
+      >
+        Save
+        <v-icon right>
+          mdi-content-save
+        </v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
@@ -65,31 +63,10 @@ export default {
 
   data: () => ({
     decks: [],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com",
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com",
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify",
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs",
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify",
-      },
-    ],
+
   }),
   created: function () {
-    // console.log("a is: " + this.a);
+    this.greet()
   },
   methods: {
     greet: function () {
@@ -166,28 +143,6 @@ export default {
 
       let decks = this.convertMapToArray(treeRootMap, idNameMap);
 
-      // items.forEach((key) => {
-      //   let deck;
-      //   let names = key.split("::");
-      //   for (let i = names.length - 1; i >= 0; i--) {
-      //     if (deck) {
-      //       deck = { name: names[i], children: [deck] };
-      //     } else {
-      //       deck = { id: data[key], name: names[i] };
-      //     }
-      //   }
-      //   // for (let i = 0, n = names.length - 1; i <= n; i++) {
-      //   //   const name = names[i];
-      //   //   if (deck?.[name]) {
-      //   //     if (i === n) {
-      //   //       deck.id = data[key];
-      //   //     }
-      //   //   } else {
-
-      //   //   }
-      //   // }
-      //   decks.push(deck);
-      // });
       return decks;
     },
     convertMapToArray(map, idNameMap) {
